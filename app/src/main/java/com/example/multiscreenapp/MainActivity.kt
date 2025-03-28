cad7c98e344bab2a30ebfb08409bf94c5553f3fe
mahesh <maheshchaparlas3445191@gmail.com>
2025-03-28 10:05:05 +0000

    check

--- app/src/main/java/com/example/multiscreenapp/MainActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MultiScreenAppTheme {
-        Greeting("Android")
+        Greeting("Android1234566")
     }
 }
